65799aab5294fc0cf646f9680e1ecafbfd22b52a
Aef Badmus <realafe21@gmail.com>
2020-05-01 00:53:32 +0100

    withdraw done including responsiveness

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const navLinks = document.querySelectorAll('.nav-links li a');
 const myCanvas = document.querySelector('.my-canvas');
+const modalClose = document.querySelectorAll('.modal-close');
 
 if (window.NodeList && !NodeList.prototype.forEach) {
   NodeList.prototype.forEach = Array.prototype.forEach;
@@ -10,6 +11,16 @@ navLinks.forEach(
       navlink.addEventListener('click', function () {
           myCanvas.classList.add(navlink.dataset.linkClass)
       })
+  },
+  'myThisArg'
+);
+
+
+modalClose.forEach(
+  function(modalLink) {
+    modalLink.addEventListener('click', function () {
+      myCanvas.className ="my-canvas"
+    })
   },
   'myThisArg'
 );
