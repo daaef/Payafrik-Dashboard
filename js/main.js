fa4f516a2495608a577767bf7fd6a264e9f74cfd
Aef Badmus <realafe21@gmail.com>
2020-05-02 17:53:56 +0100

    finished the wallet page and its modals. will update the transfer token modal next

--- js/main.js
+++ js/main.js
@@ -2,6 +2,11 @@ const navLinks = document.querySelectorAll('.nav-links li a');
 const myCanvas = document.querySelector('.my-canvas');
 const modalClose = document.querySelectorAll('.modal-close');
 
+const walletContent = document.querySelector('.wallet-content');
+const walletSidebarLinks = document.querySelectorAll('.wallet-sidebar > ul > li');
+
+const walletBtns = document.querySelectorAll('.wallet-btns button');
+
 if (window.NodeList && !NodeList.prototype.forEach) {
   NodeList.prototype.forEach = Array.prototype.forEach;
 }
@@ -10,6 +15,23 @@ navLinks.forEach(
   function(navlink) {
       navlink.addEventListener('click', function () {
           myCanvas.classList.add(navlink.dataset.linkClass)
+      })
+  },
+  'myThisArg'
+);
+
+function resetWalletModal(){
+  document.querySelectorAll('.wallet--modals>div').forEach(function (modal) {
+    console.log('removing class from modal', modal);modal.classList.remove('wallet-active');
+  });
+}
+
+walletBtns.forEach(
+  function(btn) {
+      btn.addEventListener('click', function () {
+          myCanvas.classList.add(btn.dataset.linkClass);
+          resetWalletModal();
+          document.querySelector(`.${btn.dataset.modal}`).classList.add('wallet-active')
       })
   },
   'myThisArg'
@@ -24,3 +46,24 @@ modalClose.forEach(
   },
   'myThisArg'
 );
+
+function resetWalletContentClass() {
+  walletContent.className = "wallet-content";
+}
+
+function resetWalletLinksClass() {
+  walletSidebarLinks.forEach(function (link) {
+      link.classList.remove('w-active')
+  });
+}
+
+walletSidebarLinks.forEach(function (link) {
+    var linkChild = link.querySelector('a');
+
+    linkChild.addEventListener('click', function () {
+        resetWalletContentClass();
+        walletContent.classList.add(linkChild.dataset.linkClass);
+        resetWalletLinksClass();
+        link.classList.add('w-active');
+    })
+});
